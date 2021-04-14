6df8314af40cec6a2cc65368fbeeaab853417544
Stephen Bain <swbain@gmail.com>
2021-04-13 22:37:47 -0500

    loading strategy

--- oblique_strategies.lua
+++ oblique_strategies.lua
@@ -1,7 +1,33 @@
 -- eno's oblique strategies
+--
+-- key 2: load new strategy
+
+local json = include "lib/json"
+
+local BASE_URL = "http://brianeno.needsyourhelp.org/draw"
+
+local function curl_request(url)
+    print("Requesting...", url)
+    return util.os_capture( "curl -sS --max-time 20 \"" .. url .. "\"", true)
+end
+  
+local function get_json()
+    return curl_request(BASE_URL)
+end
+
+local function get_strategy()
+    return json.decode(get_json())["strategy"]
+end
 
 function redraw()
+    screen.clear()
     screen.move(64, 32)
-    screen.text_center("hello world")
+    screen.text_center(get_strategy())
     screen.update()
+end
+
+function key(n, z)
+    if n == 2 and z == 1 then
+        redraw()
+    end
 end
